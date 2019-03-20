27d8242a4339ea4ade52c5574dc244bcbe7aaec4
anildhaker <anildhaker777@gmail.com>
2019-03-20 18:48:30 +0530

    leet to geek

---
+++ LeetCode/searchInsertPos.py
@@ -0,0 +1,13 @@
+# Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+# You may assume no duplicates in the array.
+
+# Example 1:
+
+# Input: [1,3,5,6], 5
+# Output: 2
+
+
+class Solution:
+    def searchInsert(self, nums, target):
+        return sorted(nums + [target]).index(target)
