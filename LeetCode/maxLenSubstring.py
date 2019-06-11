9455378853751a768f9b7c656128c29c88f50756
anildhaker <anildhaker777@gmail.com>
2019-06-11 18:36:18 +0530

    leettogeek

--- LeetCode/maxLenSubstring.py
+++ LeetCode/maxLenSubstring.py
@@ -15,3 +15,19 @@ class Solution:
                 l.append(length_)
                 length_ = 0 
         return max(l)
+
+
+# Second Approach
+        # left, right = 0,0
+        # longest = 0 
+        # p = set()
+        # while left < len(s) and right < len(s):
+        #     if s[right] not in p:
+        #         p.add(s[right])
+        #         right += 1 
+        #         longest = max(longest,right-left)
+        #     else:
+        #         p.remove(s[left])
+        #         left += 1
+                
+        # return longest
