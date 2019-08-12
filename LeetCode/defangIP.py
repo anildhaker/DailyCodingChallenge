98d926eed0c3457beff24b4a34476a77ca58d6ef
anildhaker <anildhaker777@gmail.com>
2019-08-12 22:41:52 +0530

    defangIPAddress

--- LeetCode/defangIP.py
+++ LeetCode/defangIP.py
@@ -3,7 +3,7 @@
 # A defanged IP address replaces every period "." with "[.]".
 
 
- def defangIPaddr(self, address: str) -> str:
-        x = address.split('.')
-        y='[.]'.join(x)
-        return y
+def defangIPaddr(address):
+      x = address.split('.')
+      y='[.]'.join(x)
+      return y
