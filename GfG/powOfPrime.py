5eb01a4f4e8d1694b495cf3c2a8478560f89ec15
anildhaker <anildhaker777@gmail.com>
2019-12-27 11:09:12 +0530

    power of p in n factorial

---
+++ GfG/powOfPrime.py
@@ -0,0 +1,24 @@
+# Finding power of prime number p in n!
+
+# eg. n = 4 p =2 --> 4! = 24 = 2*2*2*3 --> should return 4 else return Zero.
+
+# Method - 1 - Naive approach. Just count power of p from 1 to n. and add them.
+
+def powerPinNfactorial(n, p):
+  ans = 0
+  temp = p
+  while (temp <= n):
+    print("n-->",n)
+    ans += n / temp
+    print("ans-->",ans)
+    temp = temp * p
+    print("temp-->",temp)
+      
+  return ans
+  
+
+print(powerPinNfactorial(8,2))
+  
+  
+
+
